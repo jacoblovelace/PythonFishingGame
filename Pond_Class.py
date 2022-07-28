5b3fba55abb458f2f5fdaff64cea736b1cd48f8d
Coby Lovelace <jtl5jjn@virginia.edu>
2022-07-28 17:39:48 -0400

    indentation fix

--- Pond_Class.py
+++ Pond_Class.py
@@ -11,28 +11,28 @@ class Pond:
         self.fish_spots = []
         self.fish = []
 
-        def place_fish(self):
-            pond_size = self.x * self.y
+    def place_fish(self):
+        pond_size = self.x * self.y
 
-            # generate random number of fish, no more than 1/3 pond size
-            num_fish = random.randrange(2, (pond_size // 3) + 1)
+        # generate random number of fish, no more than 1/3 pond size
+        num_fish = random.randrange(2, (pond_size // 3) + 1)
 
-            # create list of valid fish based on normality
-            normalized_fish = []
-            for fish in self.fish_types:
-                for _ in fish.NORMALITY:
-                    normalized_fish.append(fish)
+        # create list of valid fish based on normality
+        normalized_fish = []
+        for fish in self.fish_types:
+            for _ in fish.NORMALITY:
+                normalized_fish.append(fish)
 
-            for _ in range(num_fish):
-                fish_spot = random.randrange(pond_size)
-                self.fish_spots.append(fish_spot)
-                self.fish.append(random.choice(normalized_fish)(random.randrange(3), fish_spot))
+        for _ in range(num_fish):
+            fish_spot = random.randrange(pond_size)
+            self.fish_spots.append(fish_spot)
+            self.fish.append(random.choice(normalized_fish)(random.randrange(3), fish_spot))
 
-        def display_pond(self):
-            print("\n||| " + ("-" * 25) + " THE POND " + ("-" * 25) + " |||")
-            for r in range(self.y):
-                for c in range(self.x):
-                    row_letter = chr(ord('A') + r)
-                    spot = str(row_letter) + str(c + 1)
-                    print("| " + str(spot), end=" ")
-                print("|")
+    def display_pond(self):
+        print("\n||| " + ("-" * 25) + " THE POND " + ("-" * 25) + " |||")
+        for r in range(self.y):
+            for c in range(self.x):
+                row_letter = chr(ord('A') + r)
+                spot = str(row_letter) + str(c + 1)
+                print("| " + str(spot), end=" ")
+            print("|")
