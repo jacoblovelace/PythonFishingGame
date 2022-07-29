6e9a568ed39971d010368c09b4e7ba72a65eaf8b
Coby Lovelace <jtl5jjn@virginia.edu>
2022-07-28 21:35:55 -0400

    some pond ideas

---
+++ ponds.py
@@ -0,0 +1,5 @@
+from Pond_Class import *
+from Fish_Classes import *
+
+sakura_pond = Pond('Sakura Pond', 6, 4, [Goldfish, Koi, Catfish])
+aspen_river = Pond('Aspen River', 6, 4, [Salmon, Bass, Trout])
